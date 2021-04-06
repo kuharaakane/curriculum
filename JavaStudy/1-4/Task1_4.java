27b560a10cc430566fff255365fce0a13e41aafd
kuharaakane <79978168+kuharaakane@users.noreply.github.com>
2021-04-06 20:27:20 +0900

    課題提出
    １−４制御文（条件分岐）
    ※再提出

--- JavaStudy/1-4/Task1_4.java
+++ JavaStudy/1-4/Task1_4.java
@@ -46,21 +46,21 @@ public class Task1_4 {
 
         // ③ 「USER_RNAME」の値のみ等しい場合。
        //     定数を使用して「 パスワードに誤りがあります。 」 と出力してください。
-       if (name.equals(USER_RNAME) && !(pass.equals(USER_PASSWORD))){
+       else if (name.equals(USER_RNAME)){
            System.out.println(CONST_MSG_ERROR_PASS);
        }
 
 
         // ④ 「USER_PASSWORD」の値のみ等しい場合。
        //定数を使用して「 名前に誤りがあります。 」と出力してください。
-       if (!(name.equals(USER_RNAME)) && pass.equals(USER_PASSWORD)){
+       else if (pass.equals(USER_PASSWORD)){
            System.out.println(CONST_MSG_ERROR_NAME);
        }
 
    
         // ⑤ 「USER_RNAME」も「USER_PASSWORD」の値も間違っていた場合。 
        //定数を使用して「 入力情報に誤りがあります。 」と出力してください。
-       if (!(name.equals(USER_RNAME)) && !(pass.equals(USER_PASSWORD))){
+       else (!(name.equals(USER_RNAME)) && !(pass.equals(USER_PASSWORD))){
         System.out.println(CONST_MSG_ERROR_INPUT);
        }
        
